a54409570eeab6e73671939f93292f81b084e288
misha <mabotov2002@gmail.com>
2024-12-15 17:26:06 +0300

    LB-31 - добавление кафки

---
+++ logbot-server/src/main/kotlin/com/vk/logbot/server/configs/KafkaConfig.kt
@@ -0,0 +1,33 @@
+package com.vk.logbot.server.configs
+
+import com.vk.logbot.commons.models.BotMessage
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+@EnableKafka
+class KafkaConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, BotMessage> {
+        val producerProps = hashMapOf<String, Any>()
+        producerProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9093"
+        producerProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        producerProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
+
+
+        return DefaultKafkaProducerFactory(producerProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, BotMessage> {
+        return KafkaTemplate(producerFactory())
+    }
+}
