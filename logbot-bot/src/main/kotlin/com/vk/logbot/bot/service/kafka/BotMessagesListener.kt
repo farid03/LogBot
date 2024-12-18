c978da7afefeedf7eef95ab9227fcc613fda24ab
Maxim Puzanov <maxizenit@gmail.com>
2024-12-18 22:26:19 +0300

    Фикс десериализации сообщений из кафки

--- logbot-bot/src/main/kotlin/com/vk/logbot/bot/service/kafka/BotMessagesListener.kt
+++ logbot-bot/src/main/kotlin/com/vk/logbot/bot/service/kafka/BotMessagesListener.kt
@@ -4,6 +4,7 @@ import com.vk.logbot.bot.service.BotApiMethodExecutor
 import com.vk.logbot.bot.service.ChatInfoService
 import com.vk.logbot.commons.models.BotMessage
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 
@@ -19,7 +20,7 @@ class BotMessagesListener(
      * Прослушивает топик сообщений для бота и отправляет пользователям сообщения.
      */
     @KafkaListener(topics = ["bot-messages"])
-    fun listenBotMessages(botMessage: BotMessage) {
+    fun listenBotMessages(@Payload botMessage: BotMessage) {
         val userIds = botMessage.userIds
         userIds.forEach { userId ->
             val chatId = chatInfoService.getChatIdByUserId(userId)
