c8c39c1597c8b91572e9557b6bf5b834e1c52de0
misha <mabotov2002@gmail.com>
2024-12-18 22:16:33 +0300

    LB-31 исправление

--- logbot-server/src/main/kotlin/com/vk/logbot/server/configs/KafkaConfig.kt
+++ logbot-server/src/main/kotlin/com/vk/logbot/server/configs/KafkaConfig.kt
@@ -3,6 +3,7 @@ package com.vk.logbot.server.configs
 import com.vk.logbot.commons.models.BotMessage
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -15,9 +16,13 @@ import org.springframework.kafka.support.serializer.JsonSerializer
 @EnableKafka
 class KafkaConfig {
 
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private val kafkaUrl: String = "localhost:9093";
+
     @Bean
     fun producerFactory(): ProducerFactory<String, BotMessage> {
         val producerProps = hashMapOf<String, Any>()
+        producerProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafkaUrl
         producerProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         producerProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
 
