9a24cf783bf64e0e6838acbda22077a97c62878d
Maxim Puzanov <maxizenit@gmail.com>
2024-12-22 04:28:32 +0300

    web: фикс отображения таблицы

--- logbot-web-jmix/src/main/kotlin/com/vk/logbot/webjmix/config/security/AnonymousRole.kt
+++ logbot-web-jmix/src/main/kotlin/com/vk/logbot/webjmix/config/security/AnonymousRole.kt
@@ -2,11 +2,12 @@ package com.vk.logbot.webjmix.config.security
 
 import io.jmix.security.role.annotation.ResourceRole
 import io.jmix.security.role.annotation.SpecificPolicy
+import io.jmix.securityflowui.role.UiMinimalPolicies
 import io.jmix.securityflowui.role.annotation.MenuPolicy
 import io.jmix.securityflowui.role.annotation.ViewPolicy
 
 @ResourceRole(name = "Anonymous role", code = AnonymousRole.CODE)
-interface AnonymousRole {
+interface AnonymousRole : UiMinimalPolicies {
 
     companion object {
         const val CODE = "anonymous-role"
